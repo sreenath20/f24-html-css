247fe0dfa72f9f0c6f522f0075e8d9567ab783c3
sreenath <sreenathbr20@gmail.com>
2024-02-27 16:52:29 +0530

    sort methods added

--- oops/opp.js
+++ oops/opp.js
@@ -21,19 +21,33 @@ function updateCarById(updateCar) {
         if (updateCar.id == car.id) {
             car.name = updateCar.name;
             car.price = updateCar.price;
-            return car;            
+            return car;
         }
     }
     return "Coud not update, car not found";
 }
 function deleteCarById(delId) {
     let location = 0;
-    for(let car of cars){
-        if(car.id = delId){
-            cars.splice(location,1);
-            return car;            
-        }        
+    for (let car of cars) {
+        if (car.id = delId) {
+            cars.splice(location, 1);
+            return car;
+        }
         location++;
     }
-    return "Could not delete card Id:"+delId;
+    return "Could not delete card Id:" + delId;
+}
+function sortCarsByPrice() {
+    return cars.sort((c1, c2) => c1.price - c2.price);
+}
+function sortCarsById() {
+    return cars.sort((c1, c2) => c1.id - c2.id);
+}
+function sortCarsByName() {
+    return cars.sort((c1, c2) => {
+        if (c1.name > c2.name) return 1;
+        if (c1.name < c2.name) return -1;
+        return 0;
+    }
+    );
 }
